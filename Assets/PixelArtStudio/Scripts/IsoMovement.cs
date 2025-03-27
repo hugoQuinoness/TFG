8f84fcc54cc9b9d0cd4c779be65e7661f9107c85
hugoQuinoness <hugoqui2003@gmail.com>
2025-03-27 09:06:57 +0100

    Update
    Player puede golpear con un área de hitbox y diseño simple de la sala inicial

--- Assets/PixelArtStudio/Scripts/IsoMovement.cs
+++ Assets/PixelArtStudio/Scripts/IsoMovement.cs
@@ -10,6 +10,12 @@ public class IsoMovement : MonoBehaviour
     [SerializeField] private float runSpeed = 8f; // Nueva variable para la velocidad de correr
     [SerializeField] private float directionSmoothing = 10f;
 
+    [Header("Attack Settings")]
+    [SerializeField] private GameObject hitArea; // Referencia al prefab HitArea
+    [SerializeField] private Transform hitAreaSpawnPoint; // Punto de aparición del HitArea
+    [SerializeField] private float hitAreaVerticalOffset = 0.5f; // Desplazamiento vertical
+    [SerializeField] private float hitAreaHorizontalOffset = 0.5f; // Desplazamiento horizontal
+
     private Animator _animator;
     private Rigidbody2D _rb;
     private Vector2 _targetDirection;
@@ -28,6 +34,8 @@ public class IsoMovement : MonoBehaviour
         GetInputDirection();
         UpdateCurrentDirection();
         UpdateAnimator();
+        UpdateHitAreaSpawnPoint(); // Actualizar la posición del hitAreaSpawnPoint
+        HandleAttack();
     }
 
     void FixedUpdate()
@@ -76,10 +84,43 @@ public class IsoMovement : MonoBehaviour
         _animator.SetFloat("Vertical", _currentDirection.y);
         _animator.SetBool("Walk", _targetDirection.magnitude > 0.1f);
         _animator.SetBool("Run", _isRunning); // Actualizar la animación de correr
+        if (_rb.velocity.x == 0 && _rb.velocity.y == 0)
+        {
+            _animator.SetTrigger("Standing");
+        }
+    }
+
+    private void UpdateHitAreaSpawnPoint()
+    {
+        // Actualizar la posición del hitAreaSpawnPoint delante del personaje con desplazamientos
+        if (_currentDirection.magnitude > 0)
+        {
+            Vector2 offset = _currentDirection.normalized * hitAreaHorizontalOffset;
+            offset.y += hitAreaVerticalOffset;
+            hitAreaSpawnPoint.position = (Vector2)transform.position + offset;
+        }
+    }
+
+    private void HandleAttack()
+    {
+        if (Input.GetMouseButtonDown(0)) // Detectar clic del botón izquierdo del mouse
+        {
+            // Instanciar el objeto HitArea en el punto de aparición
+            GameObject instantiatedHitArea = Instantiate(hitArea, hitAreaSpawnPoint.position, hitAreaSpawnPoint.rotation);
+
+            // Destruir el objeto HitArea después de 0.5 segundos
+            Destroy(instantiatedHitArea, 0.4f);
+
+            // Reproducir la animación de ataque
+            _animator.SetTrigger("Attack");
+        }
     }
 
     private string GetDebuggerDisplay()
     {
         return ToString();
     }
+
 }
+
+
