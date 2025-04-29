c0cbc68e46ee68cec51894947231f79bb39b2956
hugoQuinoness <hugoqui2003@gmail.com>
2025-04-29 09:03:02 +0200

    Update

--- Assets/Data/Scripts/Enemies/Black Wizard/WizardMovement.cs
+++ Assets/Data/Scripts/Enemies/Black Wizard/WizardMovement.cs
@@ -6,15 +6,18 @@ public class WizardMovement : MonoBehaviour
 {
     public float speed = 2f;
     public float stopDistance = 0.5f; // Distancia mínima para detenerse cerca del jugador
+    public float attackDistance = 1f; // Distancia mínima para atacar al jugador
     private Rigidbody2D rb;
     private Transform target;
     private Vector2 moveDirection;
     private Animator animator;
+    private SpriteRenderer spriteRenderer; // Referencia al componente SpriteRenderer
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>(); // Obtener el componente Animator
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Obtener el componente SpriteRenderer
     }
 
     void Start()
@@ -30,15 +33,32 @@ public class WizardMovement : MonoBehaviour
             Vector3 direction = (target.position - transform.position).normalized;
             float distance = Vector3.Distance(target.position, transform.position);
 
+
             if (distance > stopDistance)
             {
                 moveDirection = direction;
                 animator.SetBool("Run", true);
+
+                // Invertir el sprite dependiendo de la dirección
+                if (moveDirection.x > 0)
+                {
+                    spriteRenderer.flipX = false; // Mirar a la derecha
+                }
+                else if (moveDirection.x < 0)
+                {
+                    spriteRenderer.flipX = true; // Mirar a la izquierda
+                }
             }
             else
             {
                 moveDirection = Vector2.zero;
                 animator.SetBool("Run", false);
+
+                // Activar la animación de ataque si está dentro de la distancia de ataque
+                if (distance <= attackDistance)
+                {
+                    animator.SetTrigger("Attack");
+                }
             }
         }
     }
@@ -51,4 +71,6 @@ public class WizardMovement : MonoBehaviour
         }
     }
 }
+
+
 
